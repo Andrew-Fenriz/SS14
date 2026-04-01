fd4866a14a463f4b8ddaf09f8d53a51354ad3f3c
Fenriz <kastonbag552@gmail.com>
2026-04-01 16:55:09 +0300

    emp damage

---
+++ Content.Server/Emp/EmpReceiverSystem.cs
@@ -0,0 +1,35 @@
+﻿using Content.Shared.Damage;
+using Content.Shared.Damage.Systems;
+using Content.Shared.Emp;
+
+namespace Content.Server.Emp;
+
+public sealed class EmpReceiverSystem : EntitySystem
+{
+    [Dependency] private readonly DamageableSystem _damageable = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<EmpReceiverComponent, EmpPulseEvent>(OnEmpPulse);
+    }
+
+    private void OnEmpPulse(Entity<EmpReceiverComponent> ent, ref EmpPulseEvent args)
+    {
+        args.Affected = true;
+        args.Disabled = true;
+
+        var damage = ent.Comp.Damage;
+
+        if (ent.Comp.ScaleByEnergy)
+            damage *= args.EnergyConsumption;
+
+        if (damage <= 0)
+            return;
+
+        var dmg = new DamageSpecifier();
+        dmg.DamageDict.Add(ent.Comp.DamageType, damage);
+
+        _damageable.TryChangeDamage(ent.Owner, dmg);
+    }
+}
---
+++ Content.Shared/Emp/EmpReceiverComponent.cs
@@ -0,0 +1,31 @@
+﻿using Content.Shared.Damage.Prototypes;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.Emp;
+
+/// <summary>
+///     This component allows an entity to react to EMP pulses by taking damage
+///     and potentially being disabled.
+/// </summary>
+[RegisterComponent]
+[NetworkedComponent]
+[AutoGenerateComponentState]
+public sealed class EmpReceiverComponent : Component
+{
+    /// <summary>
+    ///     The base amount of damage dealt when an EMP pulse hits the entity.
+    /// </summary>
+    [DataField] [AutoNetworkedField] public float Damage = 10f;
+
+    /// <summary>
+    ///     The type of damage applied to the entity.
+    /// </summary>
+    [DataField] [AutoNetworkedField] public ProtoId<DamageTypePrototype> DamageType = "Shock";
+
+    /// <summary>
+    ///     Whether to scale the damage based on the energy consumption of the EMP pulse.
+    ///     If true, damage will be multiplied by the pulse's EnergyConsumption value.
+    /// </summary>
+    [DataField] [AutoNetworkedField] public bool ScaleByEnergy;
+}
