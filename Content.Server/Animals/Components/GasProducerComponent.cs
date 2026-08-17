c499382f5994d8bfdb2dc8f8ca05350d0abd1b79
Fenriz <kastonbag552@gmail.com>
2026-08-17 22:55:25 +0300

    add gas production

---
+++ Content.Server/Animals/Components/GasProducerComponent.cs
@@ -0,0 +1,17 @@
+﻿using Content.Server.Animals.Systems;
+using Content.Shared.Atmos;
+
+namespace Content.Server.Animals.Components;
+
+/// <summary>
+/// Produces configured gases into the producer's surrounding atmosphere.
+/// </summary>
+[RegisterComponent, Access(typeof(GasProducerSystem))]
+public sealed partial class GasProducerComponent : Component
+{
+    /// <summary>
+    /// Gases and their amounts in moles produced per successful production attempt.
+    /// </summary>
+    [DataField(required: true)]
+    public Dictionary<Gas, float> Gases = [];
+}
---
+++ Content.Server/Animals/Systems/GasProducerSystem.cs
@@ -0,0 +1,29 @@
+﻿using Content.Server.Animals.Components;
+using Content.Server.Atmos.EntitySystems;
+
+namespace Content.Server.Animals.Systems;
+
+/// <inheritdoc cref="GasProducerComponent"/>
+public sealed partial class GasProducerSystem : EntitySystem
+{
+    [Dependency] private AtmosphereSystem _atmos = default!;
+
+    [SubscribeLocalEvent]
+    private void OnProduce(Entity<GasProducerComponent> ent, ref ProductionAttemptEvent args)
+    {
+        var mixture = _atmos.GetTileMixture(args.Producer, excite: true);
+        var produced = false;
+
+        foreach (var (gas, moles) in ent.Comp.Gases)
+        {
+            if (moles <= 0f)
+                continue;
+
+            produced = true;
+            mixture?.AdjustMoles(gas, moles);
+        }
+
+        if (produced)
+            args.Produced = true;
+    }
+}
