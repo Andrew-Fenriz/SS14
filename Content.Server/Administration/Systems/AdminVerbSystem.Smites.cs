5c8e357776513447bc8187a9f2dffdd0924b38ae
Fenriz <kastonbag552@gmail.com>
2026-08-18 17:53:27 +0300

    migrate silicon to prototypes

--- Content.Server/Administration/Systems/AdminVerbSystem.Smites.cs
+++ Content.Server/Administration/Systems/AdminVerbSystem.Smites.cs
@@ -1,42 +1,29 @@
 using Content.Server.Popups;
-using Content.Server.Roles;
 using Content.Server.Tabletop;
-using Content.Shared.Actions;
 using Content.Shared.Administration;
 using Content.Shared.Administration.Prototypes;
 using Content.Shared.Damage.Systems;
 using Content.Shared.Database;
 using Content.Shared.Inventory;
 using Content.Shared.Popups;
-using Content.Shared.Silicons.Laws;
-using Content.Shared.Silicons.Laws.Components;
 using Content.Shared.Tabletop.Components;
 using Content.Shared.Verbs;
 using Content.Shared.Whitelist;
 using Robust.Shared.Map.Components;
 using Robust.Shared.Player;
-using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
 
 namespace Content.Server.Administration.Systems;
 
 public sealed partial class AdminVerbSystem
 {
-    [Dependency] private SharedActionsSystem _actions = default!;
     [Dependency] private SharedGodmodeSystem _sharedGodmodeSystem = default!;
     [Dependency] private InventorySystem _inventorySystem = default!;
     [Dependency] private PopupSystem _popupSystem = default!;
-    [Dependency] private RoleSystem _role = default!;
     [Dependency] private TabletopSystem _tabletopSystem = default!;
     [Dependency] private SharedTransformSystem _transformSystem = default!;
     [Dependency] private AdminSmiteSystem _smiteSystem = default!;
     [Dependency] private EntityWhitelistSystem _whitelistSystem = default!;
-
-    private readonly EntProtoId _actionViewLawsProtoId = "ActionViewLaws";
-    private readonly ProtoId<SiliconLawsetPrototype> _crewsimovLawset = "Crewsimov";
-
-    private readonly EntProtoId _siliconMindRole = "MindRoleSiliconBrain";
-    private const string SiliconLawBoundUserInterface = "SiliconLawBoundUserInterface";
 
     // All smite verbs have names so invokeverb works.
     private void AddSmiteVerbs(GetVerbsEvent<Verb> args)
@@ -80,37 +67,6 @@ public sealed partial class AdminVerbSystem
             Message = string.Join(": ", chessName, Loc.GetString("admin-smite-chess-dimension-description"))
         };
         args.Verbs.Add(chess);
-
-        var siliconName = Loc.GetString("admin-smite-silicon-laws-bound-name").ToLowerInvariant();
-        Verb silicon = new()
-        {
-            Text = siliconName,
-            Category = VerbCategory.Smite,
-            Icon = new SpriteSpecifier.Rsi(new("Interface/Actions/actions_borg.rsi"), "state-laws"),
-            Act = () =>
-            {
-                var userInterfaceComp = EnsureComp<UserInterfaceComponent>(args.Target);
-                _uiSystem.SetUi((args.Target, userInterfaceComp), SiliconLawsUiKey.Key, new InterfaceData(SiliconLawBoundUserInterface));
-
-                if (!HasComp<SiliconLawBoundComponent>(args.Target))
-                {
-                    EnsureComp<SiliconLawBoundComponent>(args.Target);
-                    _actions.AddAction(args.Target, _actionViewLawsProtoId);
-                }
-
-                EnsureComp<SiliconLawProviderComponent>(args.Target);
-                _siliconLawSystem.SetLaws(_siliconLawSystem.GetLawset(_crewsimovLawset).Laws, args.Target);
-
-                if (_mindSystem.TryGetMind(args.Target, out var mindId, out _))
-                    _role.MindAddRole(mindId, _siliconMindRole);
-
-                _popupSystem.PopupEntity(Loc.GetString("admin-smite-silicon-laws-bound-self"), args.Target,
-                    args.Target, PopupType.LargeCaution);
-            },
-            Impact = LogImpact.Extreme,
-            Message = string.Join(": ", siliconName, Loc.GetString("admin-smite-silicon-laws-bound-description"))
-        };
-        args.Verbs.Add(silicon);
     }
 
     private void AddPrototypeSmiteVerbs(GetVerbsEvent<Verb> args)
